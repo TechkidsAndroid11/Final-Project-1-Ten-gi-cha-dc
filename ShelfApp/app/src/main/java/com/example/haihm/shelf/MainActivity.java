06dca1e1f77f239664aa9373adc41db907f405f0
sown5ot <sonnguyenhoang2310@gmail.com>
2018-01-08 22:06:02 +0700

    Fix LoginActivity UI

--- ShelfApp/app/src/main/java/com/example/haihm/shelf/MainActivity.java
+++ ShelfApp/app/src/main/java/com/example/haihm/shelf/MainActivity.java
@@ -6,44 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    TabLayout tlLogin;
-    ViewPager vpLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupUI();
     }
 
-    private void setupUI() {
-        tlLogin = findViewById(R.id.tl_login);
-        vpLogin = findViewById(R.id.vp_login);
 
-        setupTabLayout();
-        LoginPagerAdapter loginPagerAdapter = new LoginPagerAdapter(getSupportFragmentManager());
-        vpLogin.setAdapter(loginPagerAdapter);
-        vpLogin.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tlLogin));
-    }
-
-    private void setupTabLayout() {
-        tlLogin.addTab(tlLogin.newTab().setText("Đăng nhập"));
-        tlLogin.addTab(tlLogin.newTab().setText("Đăng ký"));
-        tlLogin.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                vpLogin.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-    }
 }
