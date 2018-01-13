0f5c3a9278581f695ab8ee57a442227a7b1c6e92
haihmse04631 <haihmse04631@fpt.edu.vn>
2018-01-13 16:54:53 +0700

    update code

---
+++ ShelfApp/app/src/main/java/com/example/haihm/shelf/activity/RegisterPhoneNumber.java
@@ -0,0 +1,17 @@
+package com.example.haihm.shelf.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.haihm.shelf.R;
+
+public class RegisterPhoneNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_phone_number);
+
+
+    }
+}
